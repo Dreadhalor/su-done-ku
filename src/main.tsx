e0e1b933827d31de967912235dbe9d06281b57b5
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-10 00:18:55 -0800

    Fixed dread-ui import, but linting still is weird for vite config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './index.scss';
+import 'dread-ui/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
