75f157b365df53843c853dd935d93f149111b806
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-18 15:29:47 -0800

    Random puzzle API now uses query strings instead of different routes

--- backend/src/routes/random.ts
+++ backend/src/routes/random.ts
@@ -9,6 +9,8 @@ type Puzzle = {
   rating: string;
   puzzle: string;
 };
+const difficulties = ['easy', 'medium', 'hard'] as const;
+type DifficultySetting = (typeof difficulties)[number];
 
 // Utility function to read puzzles from a file and select a random one
 const getRandomPuzzle = (filePath: string): Promise<Puzzle> => {
@@ -27,57 +29,30 @@ const getRandomPuzzle = (filePath: string): Promise<Puzzle> => {
     });
   });
 };
-const getRandomPuzzleWithDifficulty = async (difficulty: string) => {
+const getRandomPuzzleWithDifficulty = async (difficulty: DifficultySetting) => {
   const filePath = path.join(__dirname, `../puzzles/${difficulty}.txt`);
   return await getRandomPuzzle(filePath);
 };
 
-// Route to get a random puzzle
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const difficulties = ['easy', 'medium', 'hard'];
-    const randomDifficulty =
-      difficulties[Math.floor(Math.random() * difficulties.length)];
-    const filePath = path.join(__dirname, `../puzzles/${randomDifficulty}.txt`);
-
-    const puzzle = await getRandomPuzzle(filePath);
-    res.json({ difficulty: randomDifficulty, puzzle });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred while fetching a puzzle.');
-  }
-});
-
-// Route to get an easy puzzle
-router.get('/easy', async (req: Request, res: Response) => {
-  try {
-    const difficulty = 'easy';
-    const puzzle = await getRandomPuzzleWithDifficulty(difficulty);
-    res.json({ difficulty, puzzle });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred while fetching a puzzle.');
-  }
-});
-
-// Route to get a medium puzzle
-router.get('/medium', async (req: Request, res: Response) => {
-  try {
-    const difficulty = 'medium';
-    const puzzle = await getRandomPuzzleWithDifficulty(difficulty);
-    res.json({ difficulty, puzzle });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred while fetching a puzzle.');
-  }
-});
-
-// Route to get a hard puzzle
-router.get('/hard', async (req: Request, res: Response) => {
-  try {
-    const difficulty = 'hard';
-    const puzzle = await getRandomPuzzleWithDifficulty(difficulty);
-    res.json({ difficulty, puzzle });
+    const { difficulty } = req.query;
+    if (difficulty && difficulties.includes(difficulty as DifficultySetting)) {
+      const puzzle = await getRandomPuzzleWithDifficulty(
+        difficulty as DifficultySetting,
+      );
+      res.json({ difficulty, puzzle });
+    } else {
+      // no/invalid difficulty provided, just pick a random puzzle
+      const randomDifficulty =
+        difficulties[Math.floor(Math.random() * difficulties.length)];
+      const filePath = path.join(
+        __dirname,
+        `../puzzles/${randomDifficulty}.txt`,
+      );
+      const puzzle = await getRandomPuzzle(filePath);
+      res.json({ difficulty: randomDifficulty, puzzle });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while fetching a puzzle.');
